c7098c523fc85193739a5b6f48b241e23f8f547b
Quentin Laffont <qlaffont13@gmail.com>
2026-01-20 07:05:57 +0100

    feat(client): integrate Vidstack player and serve assets from local paths

--- src/components/client/clientRoutes.ts
+++ src/components/client/clientRoutes.ts
@@ -9,4 +9,25 @@ export const ClientRoutes = () =>
       reply.type('text/html');
       return stream;
     });
+
+    fastify.get('/vidstack.js', async function (req, reply) {
+      const stream = fs.createReadStream(join(__dirname, 'vidstack.js'));
+
+      reply.type('application/javascript');
+      return stream;
+    });
+
+    fastify.get('/vidstack.theme.css', async function (req, reply) {
+      const stream = fs.createReadStream(join(__dirname, 'vidstack.theme.css'));
+
+      reply.type('text/css');
+      return stream;
+    });
+
+    fastify.get('/vidstack.video.css', async function (req, reply) {
+      const stream = fs.createReadStream(join(__dirname, 'vidstack.video.css'));
+
+      reply.type('text/css');
+      return stream;
+    });
   };
